b30df66b4608a488e6d196279c95d0391834908f
Mark Cornejo <markdom.cornejo@gmail.com>
2021-05-19 13:27:27 +0400

    updated for the back-end

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
         <footer class="bg-dark text-white p-4">
         <div>
-            <p>If you want to know more about our portal, please <a className="click" href="#">Click here</a> to sign up.</p>
-            <p>If you're already login, <a className="click" href="#">Click here</a><span className="copyright"> © 2021 Copyright: BDE</span></p>
+            <p>If you want to know more about our portal, please 
+                <Link to="/signup" style={{ textDecoration: 'none' }}>
+                    <span className="click"> Click here </span>
+                </Link> 
+                to sign up.
+            </p>
+            <p>If you're already have an account please <Link to="/login" style={{ textDecoration: 'none' }}>
+            <span className="click">login</span>
+            </Link>
+            <span className="copyright"> © 2021 Copyright: BDE</span>
+            </p>
         </div>
         </footer>
     )
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -3,30 +3,30 @@ import { Link } from 'react-router-dom';
 
 function LoginForm() {
     return (
-    <form className="text-center position-absolute" id="text-center1"> 
+    <div className="text-center position-absolute" id="text-center1"> 
     <img src="./dolphin-logo.jpeg" alt="logo" className="form-logo"/> 
     <div className="login-name">
     <h2>Log in to BDE</h2>
     <h6>using your BDE account</h6>
     </div>
-    <form>
+    <div>
         <input type="email" placeholder="Email" className="email"></input>
         <div className="spaces"></div>
         <input type="password" placeholder="Password" className='password'></input>
-     </form>
+     </div>
      <div className="little-space"></div>
-     <form className="text-start">
+     <div className="text-start">
          <input type="checkbox" className="checkbox"></input>
          <p className="checkbox">Remember me</p>
-    </form>    
+    </div>    
      <div className="little-space"></div>
-     <button type="submit" class="btn btn-primary" id="button">Log in</button>
+     <button type="submit" className="btn btn-primary" id="button">Log in</button>
      <div className="little-space"></div>
      <Link to="/signup" style={{ textDecoration: 'none' }}>
      <p className="reminder">No account yet? Sign up</p>
      </Link>
      
-</form>
+</div>
     )
 }
 
